b730d64a8e6445d97252bff367808352c37e7ec5
JDCM01 <juandavid.cortes1@utp.edu.co>
2026-05-04 23:14:33 -0500

    En el caso del manejador de strings se añaden funciones para comparar dos strings y para traducir de enteros a caracteres segun el codigo ascii, en el caso del servidor se añade una función para verificar que un nombre de usuario no se encuentre registrado

--- server.c
+++ server.c
@@ -2,10 +2,50 @@
 #include<stdlib.h>
 #include"strings_handler.h"
 #include"list_handler.h"
+#include"file_handler.h"
 /*#include<sys/socket.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<netdb.h>*/
+
+/*
+*check_user_name
+*---------------
+*Función para verificar que el cliente no este usando un nombre de usuario ya registrado previamente 
+*ira de caracter en caracter dentro del fichero de usuarios hasta encontrar un ';'
+*una vez allí comparara lo que ha leido vs el nombre de usuario que ha proporcionado el cliente
+*en caso de ser iguales retornara un 1, en caso contrario continuara leyendo caracteres
+*
+*Argumentos:
+*user_name: Nombre de usuario proporcionado por el cliente como tentativa para nombre de usuario
+*MAX_SIZE: Longitud maxima de la cadena que extraere del archivo
+*file_pointer: puntero al archivo donde guardare los nombres de los clientes registrados
+*
+*Retorna:
+*1: en caso de que el nombre de usuario ya se encuentre registrado
+*0: en caso de que no se encuentre registrado
+*/
+
+int check_user_name(char user_name[], size_t MAX_SIZE, FILE *file_pointer){
+    char actual_string[MAX_SIZE];
+    int c, comparison_result;
+    int i = 0;
+    while((c = getc(file_pointer)) != EOF){
+        if(c == ';' || c == '\n'){
+            actual_string[i] = '\0';
+            comparison_result = compare_strings(actual_string, user_name, 100);
+            if(comparison_result == 1){
+                return 1;
+            }
+            i = 0;
+        }
+        else{
+            actual_string[i] = c;    
+            i++;
+        }
+    }
+    return 0;    
+}
 
 /*
 *main
@@ -22,13 +62,54 @@
 
 void main(size_t argc, char *argv[]){
     List* stack = NULL; 
+    size_t MAX_SIZE = 100;
     char server_port[port_size];
     char server_ipv4[ipv4_size];
     char server_ipv6[ipv6_size];
+    /*
     get_arguments(server_port, argv[1], 5);
     get_arguments(server_ipv4, argv[2], 20);
     add_client(server_port, server_ipv4, server_ipv6, &stack);
     add_client("8080","172.16.48.2", "db8:acad:1::2", &stack);
     add_client("8080","172.16.48.3", "db8:acad:1::3", &stack);
     show_list(stack);
+    */
+    int result;
+    FILE *file_pointer = fopen("testing_file.txt", "a+");
+    result = check_user_name("David\0", MAX_SIZE, file_pointer);
+    rewind(file_pointer);
+    if(result == 1){
+        printf("\nEl nombre de usuario David ya esta ocupado");
+    }else{
+        printf("\nEsta disponible");
+    }
+    result = check_user_name("Daniel\0", MAX_SIZE, file_pointer);
+    rewind(file_pointer);
+    if(result == 1){
+        printf("\nEl nombre de usuario Daniel ya esta ocupado");
+    }else{
+        printf("\nEsta disponible");
+    }
+    result = check_user_name("Santiago\0", MAX_SIZE, file_pointer);
+    rewind(file_pointer);
+    if(result == 1){
+        printf("\nEl nombre de usuario Santiago ya esta ocupado");
+    }else{
+        printf("\nEsta disponible");
+    }
+    result = check_user_name("Santi\0", MAX_SIZE, file_pointer);
+    rewind(file_pointer);
+    if(result == 1){
+        printf("\nEl nombre de usuario Santi ya esta ocupado");
+    }else{
+        printf("\nEsta disponible");
+    }
+    result = check_user_name("Cristian\0", MAX_SIZE, file_pointer);
+    rewind(file_pointer);
+    if(result == 1){
+        printf("\nEl nombre de usuario Cristian ya esta ocupado");
+    }else{
+        printf("\nEsta disponible");
+    }
+    fclose(file_pointer);
 }
--- strings_handler.h
+++ strings_handler.h
@@ -18,6 +18,48 @@ void copy_string(char from_this[], char to_this[], int length){
 }
 
 /*
+*from_int_to_char
+*----------------
+*Función para traducir de entero a caracter, segun la tabla ascii
+*
+*argumentos:
+*character: caracter a traducir
+*
+*Retorno:
+*un caracter que puede ir desde la a hasta z, punto, coma, dos puntos, 
+*punto y coma, caracter nulo, salto,
+*de linea
+*/
+char from_int_to_char(int character){
+    int i;
+    /*65-89 A-Z
+    , = 44
+    . = 46
+    : = 58
+    ; = 59
+    \n = 10
+    \0 = 0*/
+    char *letters[] = {"a", "b", "c", "d", "f", "g", "h", "i", "j", 
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "\0"};
+    if(character >= 65 && character <= 89){
+        i = character - 65;
+        return *letters[i];
+    }else if(character == 44){
+        return ',';
+    }else if(character == 46){
+        return '.';
+    }else if(character == 58){
+        return ':';
+    }else if(character == 59){
+        return ';';
+    }else if(character == 10){
+        return '\n';
+    }else if(character == 0){
+        return '\0';
+    }
+}
+
+/*
 *concatenate_string
 *------------------
 *Función para agregar mas caracteres al final de una string
@@ -45,8 +87,41 @@ void concatenate_string(char this_string[], char plus_this[]){
 }
 
 /*
-* create_message
-* --------------
+*compare_strings
+*---------------
+*Función que comparara todos los caracteres de dos strings, y las recorrea hasta 
+*que suceda una de las condiciones de parada:
+*-el caracter actual analizado sea '\0' en alguna de las dos cadenas
+*-algun caracter sea distinto 
+*-se alcance la longitud máxima de las cadenas(MAX_SIZE)
+*
+*Argumentos:
+*string_a: La primera de las dos strings
+*string_b: La segunda de las dos strings
+*MAX_SIZE: valor máximo que podra tener la cadena
+*
+*Retorna:
+*1 si recorre ambas strings hasta encontrar '\0' en ambas cadenas
+*0 en cualquier otro caso
+*/
+int compare_strings(char string_a[], char string_b[], size_t MAX_SIZE){
+    int i = 0;
+    while(i < MAX_SIZE && (string_a[i] != '\0') && (string_b[i] != '\0') ){
+        if(string_a[i] != string_b[i]){
+            return 0;
+        }
+        i++;
+    }
+    if((string_a[i] == string_b[i]) && (string_a[i] == '\0')){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+/*
+* get_string
+* ----------
 * Lee una línea de entrada desde el teclado usando getchar() y la almacena
 * en el arreglo 'string'.
 *
@@ -62,7 +137,7 @@ void concatenate_string(char this_string[], char plus_this[]){
 * - message: arreglo donde se almacenará la cadena
 * - max_length: tamaño máximo del arreglo
 */
-void create_message(char message[], size_t max_length){
+void get_string(char message[], size_t max_length){
     int character;
     int i = 0;
     while(i< max_length - 1 && (character=getchar()) != EOF && character != '\n'){
